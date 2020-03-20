5a40fe8354ee342ad65e087d380b9b48baa2f8d6
Yuki Finamore <yuki@loldesign.com.br>
2020-03-20 16:20:41 -0300

    Create Ubs Card Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import Header from './components/Header';
+import UbsCard from './components/UbsCard';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+
+      <div className="ubs-list-container">
+        <UbsCard/>
+      </div>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/components/UbsCard/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles.scss";
+
+const UbsCard = (props) => (
+  <div className="ubs-card-container">
+    <Card border="light" style={{ width: '18rem' }}>
+      <Card.Header className="ubs-name">
+        1. UBS Jardim Europa
+      </Card.Header>
+
+      <Card.Body>
+        <Card.Title className="ubs-locale">
+          Rua Pascal, 1382 - Jardim Europa - SP
+        </Card.Title>
+
+        <Card.Text className="ubs-phone">
+          <b>Telefone:</b>
+          011 4157-8945         
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </div>
+);
+
+export default UbsCard
