b95b8fe08323b6258cb2fcfec0b02271458bc8f9
Yuki Finamore <yuki@loldesign.com.br>
2020-03-22 04:28:39 -0300

    Update UbsCard Component

--- src/components/UbsCard/index.js
+++ src/components/UbsCard/index.js
@@ -3,25 +3,40 @@ import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.scss";
 
-const UbsCard = (props) => (
-  <div className="ubs-card-container">
-    <Card border="light" style={{ width: '18rem' }}>
-      <Card.Header className="ubs-name">
-        1. UBS Jardim Europa
-      </Card.Header>
+const UbsCard = ({
+  name,
+  address,
+  city,
+  phone,
+  geocode,
+  onClickHospital
+}) => {
+  return (
+    <div className="ubs-card-container">
+      <Card border="light" style={{ width: '18rem' }}>
+        <Card.Header className="ubs-name">
+          <a 
+            href="#!"
+            onClick={() => onClickHospital()}
+          >
+            {name}
+          </a>
+        </Card.Header>
 
-      <Card.Body>
-        <Card.Title className="ubs-locale">
-          Rua Pascal, 1382 - Jardim Europa - SP
-        </Card.Title>
+        <Card.Body>
+          <Card.Title className="ubs-locale">
+            {address}
+            {city}
+          </Card.Title>
 
-        <Card.Text className="ubs-phone">
-          <b>Telefone:</b>
-          011 4157-8945         
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
-);
+          <Card.Text className="ubs-phone">
+            <b>Telefone:</b>
+            {phone}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
 
 export default UbsCard
