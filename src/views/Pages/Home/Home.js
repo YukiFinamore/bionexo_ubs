120209e66e33070240e7213fc990f4d6b91b6d0e
Yuki Finamore <yuki@loldesign.com.br>
2020-03-22 04:29:51 -0300

    Update Home Page with dynamic data

--- src/views/Pages/Home/Home.js
+++ src/views/Pages/Home/Home.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { getHospitals } from '../../../services/api';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import { FaSearch } from "react-icons/fa";
+import "intersection-observer";
+import { ScrollView } from "@cantonjs/react-scroll-view";
+import { getHospitals, filterByInput } from '../../../services/api';
 import Header from '../../../components/Header';
 import UbsCard from '../../../components/UbsCard';
 import GoogleMapReact from 'google-map-react';
@@ -8,8 +12,13 @@ import "./styles.scss";
 const AnyReactComponent = ({ text }) => <div className="marker-item">{text}</div>;
 
 const Home = () => {
-  const [mapCenter, setMapCenter] = useState('')
-  const [hospitals, setHospitals] = useState([])
+  const [mapCenter,         setMapCenter]         = useState('-23.5428363, -46.637257')
+  const [test,              setTest]= useState({lat: -23.5428363, lng: -46.637257})
+  const [hospitals,         setHospitals]         = useState([])
+  const [search,            setSearch]            = useState('')
+  const [hospitalsBySearch, setHospitalsBySearch] = useState([])
+  const [page,              setPage]              = useState(1)
+
 
   useEffect(() => {
     getHospitals(mapCenter)
@@ -21,22 +30,74 @@ const Home = () => {
     });
   }, [mapCenter]);
 
+  const getListByTerm = (page) => {
+    filterByInput(search, page)
+    .then(response => {
+      if (page > 1) {
+        setHospitalsBySearch(hospitalsBySearch.concat(response.data))
+      } else {
+        setHospitalsBySearch(response.data)
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  const endReached = () => {
+    getListByTerm(page + 1)
+    setPage(page + 1)
+  };
+
+  const onClickSearchByTerm = () => {
+    setPage(1)
+    getListByTerm(1)
+  }
+
   return (
     <div className="App">
       <Header/>
 
-      <div className="ubs-list-container">
-        <UbsCard/>
+      <div className="input-container">
+        <InputGroup>
+          <input 
+            type="text" 
+            onChange={(event) => setSearch(event.target.value)}
+            placeholder="Busca"
+          />
+
+          <InputGroup.Append>
+            <Button disabled={search.length < 3 ? true : false} className="search-button" variant="outline-secondary" onClick={() => onClickSearchByTerm()}>
+              <FaSearch/>
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
       </div>
 
+      <ScrollView 
+        onEndReached={search.length > 3 && endReached}
+        className="ubs-list-container"
+      >
+        {
+          hospitalsBySearch.length > 0 &&
+          hospitalsBySearch.map((hospital, index) => 
+            <UbsCard
+              key={index}
+              name={hospital.name}
+              address={hospital.address}
+              city={hospital.city}
+              phone={hospital.phone}
+              onClickHospital={() => setTest({lat: hospital.geocode.lat, lng: hospital.geocode.long})}
+            />
+          )
+        }
+      </ScrollView>
+        
       <div className="map-container">
         <GoogleMapReact
           onChange={({center}) => setMapCenter(`${center.lat}, ${center.lng}`)}
           bootstrapURLKeys={{ key: 'AIzaSyAbLNX2SRGGodG0eWxAzp5WeQGRHBD3hk8' }}
-          defaultCenter={{
-            lat: -23.5428363,
-            lng: -46.637257
-          }}
+          center={test}
           defaultZoom={15}
         >
           {
