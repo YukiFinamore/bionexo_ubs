f882ef9bc4664c11a84ff37c44675dd6a4855a1a
Yuki Finamore <yuki@loldesign.com.br>
2020-03-23 10:53:38 -0300

    Update default constant api.path to staging environment

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const constants = {
   api: {
-    path: 'http://localhost:3000',
+    // path: 'http://localhost:3000',
+    path: 'https://bionexo-ubs.herokuapp.com/',
   },
 }
 export default constants;
